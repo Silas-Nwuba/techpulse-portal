06225653b6fd362187d6d110441a0da46d35e275
Silas-Nwuba <nwubasilas@yahoo.com>
2024-06-11 22:52:19 +0100

    refactoring the code

--- src/ui/Stat.jsx
+++ src/ui/Stat.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Stat = ({ color, name, data, children }) => {
   return (
     <div
-      className={`bg-white dark:bg-[#2D3748] rounded-md py-3 flex items-center px-5  gap-4 shadow-sm w-full`}
+      className={`bg-white dark:bg-[#2D3748]  rounded-md py-5 md:py-3 flex items-center px-5  gap-4 shadow-md md:shadow-sm w-full`}
     >
       <span className={`${color} rounded-full p-4`}>{children}</span>
       <span className="leading-6">
-        <h1 className="text-stone-[#333333] dark:text-[#CBD5E0] font-medium text-[15px]">
+        <h1 className="text-stone-[#333333] dark:text-[#CBD5E0] font-medium text-lg">
           {name}
         </h1>
         <h3 className="text-stone-[#333333] dark:text-[#CBD5E0] font-semibold text-[18px] ">
